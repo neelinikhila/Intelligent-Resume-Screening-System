b3e60bb4b4ad02b8cc416674e6356e54a5e76a0e
neelinikhila <neelinikhila05@gmail.com>
2026-08-01 17:58:38 +0530

    Use SSL for Gmail SMTP

--- main.py
+++ main.py
@@ -48,8 +48,8 @@ async def send_email(to_email, subject, body):
     await aiosmtplib.send(
         message,
         hostname="smtp.gmail.com",
-        port=587,
-        start_tls=True,
+        port=465,
+        use_tls=True,
         username=os.getenv("EMAIL_USER"),
         password=os.getenv("EMAIL_PASSWORD"),
     )
